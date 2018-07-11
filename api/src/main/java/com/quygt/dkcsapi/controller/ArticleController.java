910c708c355dce55d749ffcc4e013fbebb439d0e
Sam <wxx90426@163.com>
2018-07-11 15:59:19 +0800

    update

---
+++ api/src/main/java/com/quygt/dkcsapi/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package com.quygt.dkcsapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther: wuxianxin
+ * @Date: 2018/7/11 15:58
+ * @Description:
+ */
+@RequestMapping(value = "/article", produces = "text/plain;charset=UTF-8")
+public class ArticleController extends BaseController {
+
+
+    //region 官方文章发布
+
+    //endregion
+}
